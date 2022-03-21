de6d24cbb7df1ae4e3b57db4bf90c8bbc1ed9b5b
md-Imteyaz <98022042+md-Imteyaz@users.noreply.github.com>
2022-03-21 18:13:38 +0530

    Add files via upload

---
+++ TasksMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.Tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksMicroserviceApplication.class, args);
+	}
+
+}
